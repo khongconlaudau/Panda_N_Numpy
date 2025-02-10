e61543b7c5988b2aa054da820f9eac15927a4508
Mason Van <vanducmanh266@gmail.com>
2025-02-09 20:00:24 -0500

    redo implemenataion of regularization

--- Supervised/NeuralNetworks/ModelEvaluating/utils/custom_funtion.py
+++ Supervised/NeuralNetworks/ModelEvaluating/utils/custom_funtion.py
@@ -7,7 +7,7 @@ def build_model():
     model_1 = Sequential([
         Dense(units=25, activation='relu', kernel_regularizer=L2(0.01)),
         Dense(units=15, activation='relu',kernel_regularizer=L2(0.01)),
-        Dense(units=1, kernel_regularizer=L2(0.01)),
+        Dense(units=1),
     ], name='model_1')
 
     model_2 = Sequential([
@@ -15,7 +15,7 @@ def build_model():
         Dense(units=12, activation='relu',kernel_regularizer=L2(0.01)),
         Dense(units=12, activation='relu',kernel_regularizer=L2(0.01)),
         Dense(units=20, activation='relu',kernel_regularizer=L2(0.01)),
-        Dense(units=1,kernel_regularizer=L2(0.01)),
+        Dense(units=1),
     ],name='model_2')
 
     model_3 = Sequential([
@@ -24,7 +24,7 @@ def build_model():
         Dense(units=8, activation='relu',kernel_regularizer=L2(0.01)),
         Dense(units=4, activation='relu',kernel_regularizer=L2(0.01)),
         Dense(units=12, activation='relu',kernel_regularizer=L2(0.01)),
-        Dense(units=1,kernel_regularizer=L2(0.01)),
+        Dense(units=1),
     ], name='model_3')
 
     return [model_1, model_2, model_3]
